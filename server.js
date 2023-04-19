4f6c163487877c6c8551584cccf6a2e632744a97
SanteriV <54982640+SvenZerkov@users.noreply.github.com>
2023-04-19 10:53:06 +0300

    added task list to better tackle the issues

--- server.js
+++ server.js
@@ -22,10 +22,26 @@ The dataset can be either:
 Hardcoded array of JavaScript objects (as we have done) 
 You can use (open) APIs (though creating, deleting, updating might be impossible) 
 Read the data from the JSON file (You don’t have to update the file). 
-Create your own dataset - don’t use the same we used during the lessons. There have to be more properties (not just id, name & price) AND/OR the datatype of one property is different (for example, boolean, date, object or array).
+Create your own dataset - don’t use the same we used during the lessons. There have to be more properties (not just id, name & price) 
+AND/OR the datatype of one property is different (for example, boolean, date, object or array).
 
-2. Create one server-side rendered webpage. Using Handlebars list the resources to the page. Add an external CSS file and add some styling to the webpage. You can do this using the same project.
+2. Create one server-side rendered webpage. Using Handlebars list the resources to the page. Add an external CSS file and add some styling 
+to the webpage. You can do this using the same project.
 
 
 
 Notice! Follow the instructions. Use the techniques we used in the classes (for example Express). */
+
+/* 
+- create data on separate JSON file -> done
+- create route for 
+  - get one
+  - get all
+  - create new
+  - delete 
+
+- create one server side rendered (SSR) web page => must use handlebars
+- render the content on that page
+- add css file 
+  modify the page to be more accessible
+  */
